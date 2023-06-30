778570cbb29617d8bcb75a5e671d4b509c0c3937
emb3rn <embern@protonmail.com>
2023-07-01 00:07:38 +0100

    player working

---
+++ TODO.py
@@ -0,0 +1,18 @@
+#TODO: Player
+
+#TODO: Zombies
+
+#TODO: Building
+
+#TODO: Wave and point system
+
+#TODO: Inventory and pickup
+
+#TODO: Textures
+
+#TODO: Items like guns and ammo
+
+#TODO: Add delta timing for movement, because shitty laptops might run this at like 20fps, people will be moving faster than others.
+#           - Change the fps cap to unlimited when the delta is implemented
+
+#TODO: Multiplayer
